b7d377af76779e491f5bc231d510482d702019cd
Tidiany <tidianycorporation337@gmail.com>
2023-12-22 16:59:50 +0000

    Gestion des logs...

--- src/main/java/com/groupeisi/adminapp/config/LoggingAspect.java
+++ src/main/java/com/groupeisi/adminapp/config/LoggingAspect.java
@@ -27,7 +27,7 @@ public class LoggingAspect {
     /**
      * Pointcut that matches all Spring beans in the application's main packages.
      */
-    @Pointcut("within(com.groupeisi.*)" +
+    @Pointcut("within(com.groupeisi.adminapp.*)" +
             " || within(com.groupeisi.adminapp.controller..*)")
     public void applicationPackagePointcut() {
         // Method is empty as this is just a Pointcut, the implementations are in the advices.
